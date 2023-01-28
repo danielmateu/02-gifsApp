f0fe7defb22d5fb1ce280c6f1f2f1a9f87bd0d41
Daniel Mateu <danielmateu86@gmail.com>
2023-01-28 17:40:09 +0100

    Controlar el historial de búsquedas

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -14,6 +14,8 @@ export class BusquedaComponent {
   buscar() {
 
     const valor = this.txtBuscar.nativeElement.value;
+
+    if(valor.trim().length === 0) return;
     // console.log(valor)
     this.gifsService.buscarGifs(valor);
 
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -15,10 +15,14 @@ export class GifsService {
     return [...this._historial]
   }
 
-  buscarGifs(query: string){
+  buscarGifs(query: string = ''){
     /* Adding the query to the beginning of the array. */
-    this._historial.unshift(query);
+    query = query.trim().toLowerCase();
 
+    if(!this._historial.includes(query)){
+      this._historial.unshift(query);
+      this._historial = this._historial.splice(0,10)
+    }
     console.log(this._historial)
   }
 
