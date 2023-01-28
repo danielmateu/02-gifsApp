dac9e9d9ae7b5b646bd02d2f8f8737129150b65a
Daniel Mateu <danielmateu86@gmail.com>
2023-01-28 19:28:59 +0100

    HttpParams

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientModule, HttpClient, HttpParams } from '@angular/common/http';
 import { Gif, SearchGifsResponse } from '../interface/gifs.interface';
 
 @Injectable({
@@ -9,10 +9,11 @@ export class GifsService {
 
   private apiKey: string = 'mWfciPfOHbKkyHQBRmGrBPMXJeULNHyH'
   private _historial: string[] = [];
+  private servicioUrl: string = 'https://api.giphy.com/v1/gifs'
 
   public resultados: Gif[] = []
 
-  get historial(){
+  get historial() {
     /* Returning a copy of the array. */
     return [...this._historial]
   }
@@ -26,24 +27,31 @@ export class GifsService {
 
   }
 
-  buscarGifs(query: string = ''){
+  buscarGifs(query: string = '') {
     /* Adding the query to the beginning of the array. */
     query = query.trim().toLowerCase();
 
-    if(!this._historial.includes(query)){
+    if (!this._historial.includes(query)) {
       this._historial.unshift(query);
-      this._historial = this._historial.splice(0,10)
+      this._historial = this._historial.splice(0, 10)
 
       localStorage.setItem('historial', JSON.stringify(this._historial));
     }
 
-    this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=mWfciPfOHbKkyHQBRmGrBPMXJeULNHyH&q=${query}&limit=10&offset=0&rating=g&lang=en`)
-    .subscribe((response) => {
-      // console.log(response.data)
-      this.resultados = response.data
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', '10')
+      .set('q', query);
 
-      localStorage.setItem('resultados', JSON.stringify(this.resultados));
-    })
+      // console.log(params.toString())
+
+    this.http.get<SearchGifsResponse>(`${this.servicioUrl}/search`,{params})
+      .subscribe((response) => {
+        // console.log(response.data)
+        this.resultados = response.data
+
+        localStorage.setItem('resultados', JSON.stringify(this.resultados));
+      })
   }
 
 }
